0cb9bfc8b661ace2d5b4dabe599ef858ef2e6bbf
AlexJohannson <mehedsanya@gmail.com>
2025-07-05 13:15:27 +0200

    [QHealth]: create project

---
+++ users/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class User(models.Model):
+    class Meta:
+        db_table = 'users'
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=30)
+    age = models.IntegerField()
+    date_of_birth = models.DateField()
+    height = models.IntegerField()
+    weight = models.IntegerField()
+    street = models.CharField(max_length=40)
+    house = models.IntegerField()
+    city = models.CharField(max_length=30)
+    region = models.CharField(max_length=30)
+    country = models.CharField(max_length=30)
+    gender = models.CharField(max_length=10)
+
